d9ce43b012554a179db86c94abe4148fb708bdd5
Luis Mantilla <luismantilla99@outlook.com>
2024-01-09 03:12:57 -0500

    fix issue with tokenizer path

--- constellatio/tokenizers/internal_tokenizers.py
+++ constellatio/tokenizers/internal_tokenizers.py
@@ -1,8 +1,8 @@
-import importlib.resources as pkg_resources
 from transformers import AutoTokenizer
+import pkg_resources
 
-with pkg_resources.path(
-    "constellatio.tokenizers", "mofid_llm_tokenizer"
-) as tokenizer_dir:
-    tokenizer_path_str = str(tokenizer_dir)
-    marko_tokenizer = AutoTokenizer.from_pretrained(tokenizer_path_str)
+# Construct the path to the tokenizer files within the package
+tokenizer_path = pkg_resources.resource_filename('constellatio', 'tokenizers/mofid_llm_tokenizer')
+
+# Load the tokenizer
+marko_tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
